91693256b4065612910d32f4e099f0888d638c83
Andreas Beyer <andreasbeyer@andreass-mbp.speedport.ip>
2020-01-03 23:41:25 +0100

    update

--- util/createContext.js
+++ util/createContext.js
@@ -7,17 +7,12 @@ const createContext = async ({ req, res }) => {
     req.headers.authorization &&
     req.headers.authorization.replace("Bearer ", "");
   try {
-    if (!token) {
-      // eslint-disable-next-line no-console
-      console.log("jwt verification failed");
-      throw new Error("jwt missing");
-    }
     const { data } = jwt.verify(token, keys.jwtSecret);
     const currentUser = await User.gen(data.userId);
     return { currentUser };
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.log("jwt verification failed");
+    console.log("jwt verification failed", err);
     res.send(
       `{"errors":[{"extensions":{"code":"UNAUTHENTICATED"},"message":"INVALID_JWT"}]}`
     ); // make sure the client understands that there is some problem with authentication
