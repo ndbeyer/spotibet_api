d125e57d4cfeac2bc13f04c6f4cae4ce876b2f87
Andreas Beyer <abeyer@posteo.de>
2020-05-08 19:20:48 +0200

    [api] update

--- queries/makeEndedBetTransactions.js
+++ queries/makeEndedBetTransactions.js
@@ -3,13 +3,18 @@ const axios = require("axios");
 const keys = require("../config/keys");
 const lodash = require("lodash");
 
-const getEndedBets = async input => {
+const getEndedBets = async (input) => {
   const ids = Array.isArray(input) ? input : [input];
   return (
     await db.query(
-      `SELECT * FROM public.bet WHERE transactions = false AND (now() at time zone 'utc') > end_date ${
-        input ? "AND id = ANY($1)" : ""
-      }`,
+      `SELECT 
+        artist_id AS artistId, 
+        end_date::text AS endDate
+        FROM public.bet 
+        WHERE transactions = false 
+        AND (now() at time zone 'utc') > end_date ${
+          input ? "AND id = ANY($1)" : ""
+        }`,
       input ? [ids] : []
     )
   ).rows;
@@ -18,11 +23,11 @@ const getEndedBets = async input => {
 const makeEndedBetTransactions = async (input) => {
   try {
     const endedBets = await getEndedBets(input);
-    console.log("endedBets", endedBets)
+    console.log("endedBets", endedBets);
     if (!endedBets.length) {
       return { success: true };
     }
-    const betsPerArtist = lodash.groupBy(endedBets, "artist_id");
+    const betsPerArtist = lodash.groupBy(endedBets, "artistId");
     // go through all artist-grouped bets, get stats for each artist
     for (const artistId in betsPerArtist) {
       // eslint-disable-next-line no-await-in-loop
@@ -30,16 +35,16 @@ const makeEndedBetTransactions = async (input) => {
         `${keys.statServerURI}/artist:${artistId}`,
         {
           headers: {
-            Authorization: keys.statServerSecret
-          }
+            Authorization: keys.statServerSecret,
+          },
         }
       );
       const bets = betsPerArtist[artistId];
       // go through all bets of each artist and determine the listeners_at_end_date
       // eslint-disable-next-line no-await-in-loop
       await Promise.all(
-        bets.map(async bet => {
-          const endDate = bet.end_date.toISOString().substr(0, 10);
+        bets.map(async (bet) => {
+          const endDate = bet.endDate.substr(0, 10);
           const endDateEntry =
             data &&
             data.payload &&
@@ -81,7 +86,7 @@ const makeEndedBetTransactions = async (input) => {
 
           const serviceFee = lostMoney * 0.1;
           const lostMoneyToDistribute = lostMoney - serviceFee;
-          const cashPerWinnerAmount = lostMoneyToDistribute / allWinnerAmounts;
+          const winnersFactor = lostMoneyToDistribute / allWinnerAmounts;
 
           // !!! updating user.money, transactions and bet.transactions must be done in one query
 
@@ -89,7 +94,7 @@ const makeEndedBetTransactions = async (input) => {
           await Promise.all(
             winningParticipants.map(async ({ amount, user_id }) => {
               const addedCash =
-                Number(amount) + Number(amount) * Number(cashPerWinnerAmount);
+                Number(amount) + Number(amount) * Number(winnersFactor);
               await Promise.all([
                 await db.query(
                   "UPDATE public.user SET money = money + $1 WHERE id = $2",
@@ -98,7 +103,7 @@ const makeEndedBetTransactions = async (input) => {
                 await db.query(
                   "INSERT INTO public.transaction (amount, user_id, type, bet_id, datetime) VALUES ($1, $2, $3, $4, now())",
                   [addedCash, user_id, "PLUS", bet.id]
-                )
+                ),
               ]);
             })
           );
