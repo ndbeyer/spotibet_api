d29cec57cedd61494ed654eef510efe7996313c7
Andreas Beyer <abeyer@posteo.de>
2020-07-09 12:04:15 +0200

    add addArtistToStatServer

--- classes/Artist.js
+++ classes/Artist.js
@@ -2,7 +2,41 @@ const getSpotifyData = require("../util/getSpotifyData");
 const { db } = require("../db");
 const getMonthlyListeners = require("../util/getMonthlyListeners");
 const syncArtistMonthlyListenersHistory = require("../mutations/syncArtistMonthlyListenersHistory");
+const fetch = require("node-fetch");
+
+const keys = require("../config/keys");
 // require other classes after exports to avoid circular dependencies
+
+const addArtistsToStatServer = async (artists) => {
+  try {
+    const res = await fetch(`${keys.statServerURI}/insert`, {
+      method: "post",
+      body: JSON.stringify({
+        inserts: artists.map((artist) => ({
+          artist_id: artist.id,
+          spotify_url: artist.spotifyUrl,
+          artist_name: artist.name,
+        })),
+      }),
+      headers: {
+        Authorization: keys.statServerSecret,
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.status !== 200) {
+      console.log("...addArtistsToStatServer: res.status !== 200");
+      return;
+    }
+    const { success, error } = await res.json();
+    if (!success) {
+      console.log("...addArtistsToStatServer: success = false");
+      return;
+    }
+    console.log(`Added ${artists.length} artists to statServer`);
+  } catch (e) {
+    console.log("...addArtistsToStatServer: catch error", e);
+  }
+};
 
 module.exports = class Artist {
   constructor(data) {
@@ -11,7 +45,8 @@ module.exports = class Artist {
 
   static async gen(spotifyArtistId, currentUser) {
     const data = await getSpotifyData(currentUser, "artists", spotifyArtistId);
-    return data.map(
+
+    const artist = data.map(
       ({ id, name, images, popularity, followers, external_urls }) =>
         new Artist({
           id,
@@ -22,12 +57,15 @@ module.exports = class Artist {
           spotifyUrl: external_urls ? external_urls.spotify : null,
         })
     )[0];
+
+    addArtistsToStatServer([artist]);
+    return artist;
   }
 
   static async genMult(spotifyArtistIds, currentUser) {
     const data = await getSpotifyData(currentUser, "artists", spotifyArtistIds);
 
-    return data.map(
+    const artists = data.map(
       ({ id, name, images, popularity, followers, external_urls }) =>
         new Artist({
           id,
@@ -38,6 +76,9 @@ module.exports = class Artist {
           spotifyUrl: external_urls ? external_urls.spotify : null,
         })
     );
+
+    addArtistsToStatServer(artists);
+    return artists;
   }
 
   async monthlyListeners() {
