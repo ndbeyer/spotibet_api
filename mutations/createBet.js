20dd888bb2260addee7bd2674836b74ed82b32c6
Andreas Beyer <abeyer@posteo.de>
2020-07-23 10:39:06 +0200

    upgrade joinBet and createBet errors

--- mutations/createBet.js
+++ mutations/createBet.js
@@ -1,3 +1,5 @@
+const { ApolloError } = require("apollo-server-express");
+
 const { db } = require("../db");
 const axios = require("axios");
 const User = require("../classes/User");
@@ -9,49 +11,45 @@ const createBet = async (
   { artistId, artistName, spotifyUrl, listeners, type, endDate },
   currentUser
 ) => {
-  try {
-    const betTimer = new BetTimer();
-    const valid = betTimer.validate(endDate);
-    if (!valid) {
-      throw new Error("Invalid betTiming");
-    }
-    const startDate = betTimer.starts("iso");
-    // GENERATE BET ENTRY
-    const userIdDb = User.decryptId(currentUser.id);
-    const betId = (
-      await db.query(
-        `INSERT INTO public.bet (user_id, artist_id, spotify_url, listeners, type, start_date, end_date, transactions) VALUES ($1, $2, $3, $4, $5, $6, $7, false)
-       RETURNING id`,
-        [userIdDb, artistId, spotifyUrl, listeners, type, startDate, endDate]
-      )
-    ).rows[0].id;
-    // SEND ARTIST TO STAT_SERVER
-    const { data } = await axios.post(
-      `${keys.statServerURI}/insert`,
-      {
-        inserts: [
-          {
-            artist_id: artistId,
-            spotify_url: spotifyUrl,
-            artist_name: artistName,
-          },
-        ],
-      },
-      {
-        headers: {
-          Authorization: keys.statServerSecret,
-        },
-      }
-    );
-    if (!data.success) {
-      await db.query(`DELETE FROM public.bet WHERE id = $1`, [betId]);
-      throw new Error("error during sending artist to statServer");
-    }
-    const bet = await Bet.gen(betId);
-    return { bet };
-  } catch (e) {
-    throw new Error(e);
+  const betTimer = new BetTimer();
+  const valid = betTimer.validate(endDate);
+  if (!valid) {
+    throw new ApolloError("INVALID_BET_TIMING");
   }
+  const startDate = betTimer.starts("iso");
+  // GENERATE BET ENTRY
+  const userIdDb = User.decryptId(currentUser.id);
+  const betId = (
+    await db.query(
+      `INSERT INTO public.bet (user_id, artist_id, spotify_url, listeners, type, start_date, end_date, transactions) VALUES ($1, $2, $3, $4, $5, $6, $7, false)
+     RETURNING id`,
+      [userIdDb, artistId, spotifyUrl, listeners, type, startDate, endDate]
+    )
+  ).rows[0].id;
+  // SEND ARTIST TO STAT_SERVER
+  const { data } = await axios.post(
+    `${keys.statServerURI}/insert`,
+    {
+      inserts: [
+        {
+          artist_id: artistId,
+          spotify_url: spotifyUrl,
+          artist_name: artistName,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: keys.statServerSecret,
+      },
+    }
+  );
+  if (!data.success) {
+    await db.query(`DELETE FROM public.bet WHERE id = $1`, [betId]);
+    throw new ApolloError("STAT_SERVER_ERROR");
+  }
+  const bet = await Bet.gen(betId);
+  return { bet };
 };
 
 module.exports = createBet;
--- mutations/joinBet.js
+++ mutations/joinBet.js
@@ -1,3 +1,4 @@
+const { ApolloError } = require("apollo-server-express");
 const { db } = require("../db");
 const User = require("../classes/User");
 const Bet = require("../classes/Bet");
@@ -14,39 +15,35 @@ const valiDateJoinDate = async (betIdDb) => {
 };
 
 const joinBet = async ({ betId, support, amount }, currentUser) => {
-  try {
-    const userIdDb = User.decryptId(currentUser.id);
-    const betIdDb = Bet.decryptId(betId);
-    //make sure the bet is still joinable
-    const betIsJoinable = await valiDateJoinDate(betIdDb);
-    if (!betIsJoinable) throw new Error("BET_NOT_JOINABLE");
-    // make sure the user has enough money
-    const moneyBefore = (
-      await db.query(`SELECT money FROM public.user WHERE id = $1`, [userIdDb])
-    ).rows[0].money;
-    if (moneyBefore < amount) throw new Error("NOT_ENOUGH_MONEY");
-    // subtract money from currentUser
-    await db.query(`UPDATE public.user SET money = money - $1 WHERE id = $2`, [
-      amount,
-      userIdDb,
-    ]);
-    // make transaction entry
-    const response = await db.query(
-      "INSERT INTO public.transaction (amount, user_id, type, bet_id, datetime) VALUES ($1, $2, $3, $4, now()) RETURNING id",
-      [amount, userIdDb, "MINUS", betIdDb]
-    );
-    const transactionId = response.rows[0].id;
-    const transaction = await Transaction.gen(transactionId);
-    // create participation
-    await db.query(
-      "INSERT INTO public.participant (bet_id, user_id, support, amount) VALUES ($1, $2, $3, $4) RETURNING id",
-      [betIdDb, userIdDb, support, amount]
-    );
-    const bet = await Bet.gen(betId);
-    return { bet, transaction };
-  } catch (e) {
-    throw new Error(e);
-  }
+  const userIdDb = User.decryptId(currentUser.id);
+  const betIdDb = Bet.decryptId(betId);
+  //make sure the bet is still joinable
+  const betIsJoinable = await valiDateJoinDate(betIdDb);
+  if (!betIsJoinable) throw new ApolloError("BET_NOT_JOINABLE");
+  // make sure the user has enough money
+  const moneyBefore = (
+    await db.query(`SELECT money FROM public.user WHERE id = $1`, [userIdDb])
+  ).rows[0].money;
+  if (moneyBefore < amount) throw new ApolloError("NOT_ENOUGH_MONEY");
+  // subtract money from currentUser
+  await db.query(`UPDATE public.user SET money = money - $1 WHERE id = $2`, [
+    amount,
+    userIdDb,
+  ]);
+  // make transaction entry
+  const response = await db.query(
+    "INSERT INTO public.transaction (amount, user_id, type, bet_id, datetime) VALUES ($1, $2, $3, $4, now()) RETURNING id",
+    [amount, userIdDb, "MINUS", betIdDb]
+  );
+  const transactionId = response.rows[0].id;
+  const transaction = await Transaction.gen(transactionId);
+  // create participation
+  await db.query(
+    "INSERT INTO public.participant (bet_id, user_id, support, amount) VALUES ($1, $2, $3, $4) RETURNING id",
+    [betIdDb, userIdDb, support, amount]
+  );
+  const bet = await Bet.gen(betId);
+  return { bet, transaction };
 };
 
 module.exports = joinBet;
