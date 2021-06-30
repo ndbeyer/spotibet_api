9b90fa16c88e3c8f37e83c033857faaa7f0be219
Andreas Beyer <abeyer@posteo.de>
2021-06-30 21:13:53 +0200

    fix getMonthlylistener

--- util/getMonthlyListeners.js
+++ util/getMonthlyListeners.js
@@ -1,16 +1,31 @@
 const getBody = require("./getBody");
 const cheerio = require("cheerio");
 
+const log = true
+
 const getMonthlyListeners = async (spotifyUrl) => {
-  const artistUrl = `${spotifyUrl}/about`;
+  const artistUrl = `${spotifyUrl}`;
   const body = await getBody(artistUrl);
   const $ = cheerio.load(body);
-  const monthlyListeners = $(".insights__column__number")
-    .map(function() {
-      return $(this).text();
-    })
-    .get()[0];
-  return monthlyListeners ? Number(monthlyListeners.replace(",", "")) : null;
+  const result = $('meta[name="description"]').attr('content')
+  if (!result) return null
+  const splitted = result.split(" ")
+  const listenersText = splitted[splitted.length - 3]
+  const multiplicator = listenersText.includes('K') ? 1000 : listenersText.includes('M') ? 1000 * 1000 : 1
+  const hasPoint = listenersText.includes('.')
+  const listenersNumber = Number(listenersText.replace('K', '').replace('M', '').replace(".", ''))
+  const listeners = hasPoint ? listenersNumber * multiplicator / 10 : listenersNumber * multiplicator
+
+  if (log) {
+    console.log('>>> result', result)
+    console.log('>>> listenersText', listenersText)
+    console.log('>>> multiplicator', multiplicator)
+    console.log('>>> listenersNumber', listenersNumber)
+    console.log('>>> listeners', listeners)
+  }
+
+  return listeners
+
 };
 
 module.exports = getMonthlyListeners
